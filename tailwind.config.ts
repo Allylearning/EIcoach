f4d476bc727a3394423d801ea9af9f423477f191
Ally Lundie <ally.lundie.co.uk>
2025-05-21 10:33:09 +0100

    font size edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,6 +61,9 @@ export default {
   				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
+  		fontSize: {
+  			sm: ['1rem', '1.5rem'],
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
